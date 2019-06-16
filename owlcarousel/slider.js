e192ea825fed363a0b16617455de0680f2d88f21
TheRelow <relow20@gmail.com>
2019-06-17 03:28:46 +0400

    First commit

---
+++ owlcarousel/slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('#partners').owlCarousel({
+  	items: 4,
+  	margin: 20,
+  	loop: true,
+  	nav: true,
+  	navText: ["",""],
+  	mouseDrag: false,
+  	touchDrag: false
+  });
+});
+
+$(document).ready(function(){
+  $('#reviews').owlCarousel({
+  	items: 3,
+  	margin: 20,
+    autoWidth: true,
+  	loop: true,
+  	nav: true,
+  	navText: ["",""],
+  	mouseDrag: false,
+  	touchDrag: false
+  });
+});
+
+$(document).ready(function(){
+  $('#about').owlCarousel({
+  	items: 3,
+  	margin: 20,
+  	loop: true,
+  	nav: true,
+  	navText: ["",""],
+  	mouseDrag: false,
+  	touchDrag: false
+  });
+});
